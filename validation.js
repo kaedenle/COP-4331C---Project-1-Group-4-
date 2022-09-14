36a7e7b2e6a66d73a7b885f64e41fcea1c6bc00f
Cristian Gutierrez <56163758+CGutii@users.noreply.github.com>
2022-09-14 13:23:41 -0400

    Create validation.js
    validation w/regex in JavaScript for the email and phone number

---
+++ validation.js
@@ -0,0 +1,34 @@
+// Javascript Field Validation
+
+/**
+ * Validates an email address stored as a string.
+ * @param {string} emailAddr String being validated
+ * @returns Boolean dependent on the validity of emailAddr
+ */
+function validateEmail(emailAddr)
+{
+    let format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (format.test(emailAddr))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+/**
+ * Validates a phone number stored as a string.
+ * @param {string} phoneNum String being validated
+ * @returns Boolean dependent on the validity of phoneNum
+ */
+function validatePhone(phoneNum)
+{
+    let format = /^\d{10}$/;
+    if (format.test(phoneNum))
+    {
+        return true;
+    }
+
+    return false;
+}
+
